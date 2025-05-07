401d1261ebf1478b7e4cf3694e17fe84ce6f85a7
Vinz Myko Del Rosario <vinzmykodelrosario@gmail.com>
2025-05-07 19:17:14 +0100

    feat: implement calculator styling

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use eframe::egui;
 
 fn main() -> Result<(), eframe::Error> {
     let viewport = egui::ViewportBuilder::default()
-        .with_inner_size([400.0, 300.0])
+        .with_inner_size([160.0, 300.0])
         .with_title("Calculator");
     
     let options = eframe::NativeOptions {
@@ -39,7 +39,19 @@ impl Default for MyApp {
 
 impl MyApp {
     fn calculator_button(&mut self, ui: &mut egui::Ui, label: &str) -> bool {
-        let button = ui.button(label);
+        let button = ui.add(egui::Button::new(
+            egui::RichText::new(label)
+                .size(24.0)
+                .text_style(egui::TextStyle::Button)
+        )
+            .min_size(egui::vec2(35.0, 35.0))
+            .rounding(egui::Rounding::same(2.5))
+        );
+
+        if button.hovered() {
+            ui.ctx().set_cursor_icon(egui::CursorIcon::PointingHand);
+        }
+
         if button.clicked() && self.reset_flag {
             if self.input == "0".to_string() && label == "0" {
 
@@ -62,16 +74,25 @@ impl eframe::App for MyApp {
             ui.heading("Basic Calculator");
 
             ui.add(egui::TextEdit::singleline(&mut self.input)
-                .font(egui::TextStyle::Monospace)
-                .desired_width(f32::INFINITY));
+                .min_size(egui::vec2(50.0, 35.0))
+                .font(egui::FontId::new(36.0, egui::FontFamily::Monospace))
+            );
 
             egui::Grid::new("calculator_grid")
-                .spacing([10.0, 10.0])
+                .spacing([5.0, 5.0])
                 .show(ui, |ui| {
                     self.calculator_button(ui, "7");
                     self.calculator_button(ui, "8");
                     self.calculator_button(ui, "9");
-                    if ui.button("C").clicked() {
+
+                    if ui.add(egui::Button::new(
+                        egui::RichText::new("C")
+                            .size(24.0)
+                    )
+                        .min_size(egui::vec2(35.0, 35.0))
+                        .rounding(egui::Rounding::same(2.5))
+                        .fill(egui::Color32::from_rgb(80, 30, 30))
+                    ).clicked() {
                         self.input = String::from("0"); 
                         self.decimal_flag = false;
                         self.stored_number = 0.0;
@@ -81,7 +102,13 @@ impl eframe::App for MyApp {
                     self.calculator_button(ui, "4");
                     self.calculator_button(ui, "5");
                     self.calculator_button(ui, "6");
-                    if ui.button("+").clicked() {
+                    if ui.add(egui::Button::new(
+                        egui::RichText::new("+")
+                            .size(24.0)
+                    )
+                        .min_size(egui::vec2(35.0, 35.0))
+                        .rounding(egui::Rounding::same(2.5))
+                    ).clicked() {
                         self.calculation_flag = '+';
                         match self.input.parse() {
                             Ok(num) => {
@@ -98,7 +125,13 @@ impl eframe::App for MyApp {
                     self.calculator_button(ui, "1");
                     self.calculator_button(ui, "2");
                     self.calculator_button(ui, "3");
-                    if ui.button("-").clicked() {
+                    if ui.add(egui::Button::new(
+                        egui::RichText::new("-")
+                            .size(24.0)
+                    )
+                        .min_size(egui::vec2(35.0, 35.0))
+                        .rounding(egui::Rounding::same(2.5))
+                    ).clicked() {
                         self.calculation_flag = '-';
                         self.stored_number = self.input.parse::<f64>().unwrap();
                         self.input = 0.to_string();
@@ -107,8 +140,20 @@ impl eframe::App for MyApp {
                     }
                     ui.end_row();
                     self.calculator_button(ui, "0");
-                    if ui.button(" ").clicked() { }
-                    if ui.button(".").clicked() {
+                    if ui.add(egui::Button::new(
+                        egui::RichText::new(" ")
+                            .size(24.0)
+                    )
+                        .min_size(egui::vec2(35.0, 35.0))
+                        .rounding(egui::Rounding::same(2.5))
+                    ).clicked() { }
+                    if ui.add(egui::Button::new(
+                        egui::RichText::new(".")
+                            .size(24.0)
+                    )
+                        .min_size(egui::vec2(35.0, 35.0))
+                        .rounding(egui::Rounding::same(2.5))
+                    ).clicked() {
                         if self.decimal_flag {
                             return;
                         }
@@ -116,7 +161,13 @@ impl eframe::App for MyApp {
                         self.decimal_flag = true;
                         self.reset_flag = false;
                     }
-                    if ui.button("=").clicked() {
+                    if ui.add(egui::Button::new(
+                        egui::RichText::new("=")
+                            .size(24.0)
+                    )
+                        .min_size(egui::vec2(35.0, 35.0))
+                        .rounding(egui::Rounding::same(2.5))
+                    ).clicked() {
                         let input_num = self.input.parse::<f64>().unwrap();
                         let mut result: f64 = 0.0;
                         match self.calculation_flag {
